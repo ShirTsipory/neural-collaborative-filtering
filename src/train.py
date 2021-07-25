730cfb6b7cef60b97683c8b6d0149db583413d1f
shirtsipory <shirtsipory@gmail.com>
2021-07-25 16:21:33 +0300

    Goodbooks data.

--- src/train.py
+++ src/train.py
@@ -8,6 +8,7 @@ from data import SampleGenerator
 
 # os.environ['CUDA_VISIBLE_DEVICES'] = "1"
 
+"""
 gmf_config = {'alias': 'gmf_factor8neg4-implict',
               'num_epoch': 50,
               'batch_size': 1024,
@@ -65,10 +66,131 @@ neumf_config = {'alias': 'pretrain_neumf_factor8neg4',
                 'pretrain_mlp': 'checkpoints/{}'.format('mlp_factor8neg4_bz256_166432168_pretrain_reg_0.0000001_Epoch49_HR0.4790_NDCG0.6568.model'),
                 'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'
                 }
+"""
+"""
+gmf_config = {'alias': 'gmf_factor8neg4_amazonbeauty',
+              'num_epoch': 50,
+              'batch_size': 1024,
+              # 'optimizer': 'sgd',
+              # 'sgd_lr': 1e-3,
+              # 'sgd_momentum': 0.9,
+              # 'optimizer': 'rmsprop',
+              # 'rmsprop_lr': 1e-3,
+              # 'rmsprop_alpha': 0.99,
+              # 'rmsprop_momentum': 0,
+              'optimizer': 'adam',
+              'adam_lr': 1e-3,
+              'num_users': 1210271,
+              'num_items': 249274,
+              'latent_dim': 8,
+              'num_negative': 4,
+              'l2_regularization': 0, # 0.01
+              'use_cuda': False,
+              'device_id': 0,
+              'model_dir':'checkpoints/amazonbeauty_{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
+
+mlp_config = {'alias': 'mlp_factor8neg4_pretrain_amazonbeauty',
+              'num_epoch': 50,
+              'batch_size': 256,  # 1024,
+              'optimizer': 'adam',
+              'adam_lr': 1e-3,
+              'num_users': 1210271,
+              'num_items': 249274,
+              'latent_dim': 8,
+              'num_negative': 4,
+              'layers': [16,64,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
+              'l2_regularization': 0.0000001,  # MLP model is sensitive to hyper params
+              'use_cuda': True,
+              'device_id': 0,
+              'pretrain': True,
+              'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4-implict_Epoch49_HR0.4618_NDCG0.6392.model'),
+              'model_dir':'checkpoints/amazonbeauty_{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
+
+neumf_config = {'alias': 'pretrain_neumf_factor8neg4_amazonbeauty',
+                'num_epoch': 50,
+                'batch_size': 1024,
+                'optimizer': 'adam',
+                'adam_lr': 1e-3,
+                'num_users': 1210271,
+                'num_items': 249274,
+                'latent_dim_mf': 8,
+                'latent_dim_mlp': 8,
+                'num_negative': 4,
+                'layers': [16,64,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
+                'l2_regularization': 0.01,
+                'use_cuda': True,
+                'device_id': 0,
+                'pretrain': True,
+                'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4-implict_Epoch49_HR0.4618_NDCG0.6392.model'),
+                'pretrain_mlp': 'checkpoints/{}'.format('mlp_factor8neg4_bz256_166432168_pretrain_reg_0.0000001_Epoch49_HR0.4790_NDCG0.6568.model'),
+                'model_dir':'checkpoints/amazonbeauty_{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'
+                }
+"""
+
+gmf_config = {'alias': 'gmf_factor8neg4_goodbooks',
+              'num_epoch': 50,
+              'batch_size': 1024,
+              # 'optimizer': 'sgd',
+              # 'sgd_lr': 1e-3,
+              # 'sgd_momentum': 0.9,
+              # 'optimizer': 'rmsprop',
+              # 'rmsprop_lr': 1e-3,
+              # 'rmsprop_alpha': 0.99,
+              # 'rmsprop_momentum': 0,
+              'optimizer': 'adam',
+              'adam_lr': 1e-3,
+              'num_users': 53424,
+              'num_items': 10000,
+              'latent_dim': 8,
+              'num_negative': 4,
+              'l2_regularization': 0, # 0.01
+              'use_cuda': False,
+              'device_id': 0,
+              'model_dir':'checkpoints/goodbooks_{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
+
+mlp_config = {'alias': 'mlp_factor8neg4_pretrain_goodbooks',
+              'num_epoch': 50,
+              'batch_size': 256,  # 1024,
+              'optimizer': 'adam',
+              'adam_lr': 1e-3,
+              'num_users': 53424,
+              'num_items': 10000,
+              'latent_dim': 8,
+              'num_negative': 4,
+              'layers': [16,64,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
+              'l2_regularization': 0.0000001,  # MLP model is sensitive to hyper params
+              'use_cuda': True,
+              'device_id': 0,
+              'pretrain': True,
+              'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4-implict_Epoch49_HR0.4618_NDCG0.6392.model'),
+              'model_dir':'checkpoints/goodbooks_{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
+
+neumf_config = {'alias': 'pretrain_neumf_factor8neg4_goodbooks',
+                'num_epoch': 50,
+                'batch_size': 1024,
+                'optimizer': 'adam',
+                'adam_lr': 1e-3,
+                'num_users': 53424,
+                'num_items': 10000,
+                'latent_dim_mf': 8,
+                'latent_dim_mlp': 8,
+                'num_negative': 4,
+                'layers': [16,64,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
+                'l2_regularization': 0.01,
+                'use_cuda': True,
+                'device_id': 0,
+                'pretrain': True,
+                'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4-implict_Epoch49_HR0.4618_NDCG0.6392.model'),
+                'pretrain_mlp': 'checkpoints/{}'.format('mlp_factor8neg4_bz256_166432168_pretrain_reg_0.0000001_Epoch49_HR0.4790_NDCG0.6568.model'),
+                'model_dir':'checkpoints/goodbooks_{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'
+                }
 
 # Load Data
-ml1m_dir = 'data/ml-1m/ratings.dat'
-ml1m_rating = pd.read_csv(ml1m_dir, sep='::', header=None, names=['uid', 'mid', 'rating', 'timestamp'],  engine='python')
+# ml1m_dir = 'data/ml-1m/ratings.dat'
+# ml1m_dir = 'data/amazonbeauty/amazonbeauty_corpus.csv'
+ml1m_dir = 'data/goodbooks/goodbooks_corpus.csv'
+# ml1m_rating = pd.read_csv(ml1m_dir, sep='::', header=None, names=['uid', 'mid', 'rating', 'timestamp'],  engine='python')
+ml1m_rating = pd.read_csv(ml1m_dir, sep=',', header=None, names=['uid', 'mid', 'rating', 'timestamp'],  engine='python')
 # Reindex
 user_id = ml1m_rating[['uid']].drop_duplicates().reindex()
 user_id['userId'] = np.arange(len(user_id))
