04229205ef9df0767c6ffa6a2fbf5d917a9d154a
shirtsipory <shirtsipory@gmail.com>
2021-08-09 08:09:58 +0300

    moviesdat 1.

--- src/args.py
+++ src/args.py
@@ -192,13 +192,14 @@ gmf_config = {'alias': 'gmf_netflix',
               # 'rmsprop_momentum': 0,
               'optimizer': 'adam',
               'adam_lr': 1e-3,
-              'num_users': 18645,
-              'num_items': 9851,
+              'num_users': 10677,
+              'num_items': 2121,
               'latent_dim': 8,
               'num_negative': 4,
               'l2_regularization': 0, # 0.01
               'use_cuda': True,
               'device_id': 0,
+              'device_id_2': 0,
               'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
 
 mlp_config = {'alias': 'mlp_netflix',
@@ -206,16 +207,17 @@ mlp_config = {'alias': 'mlp_netflix',
               'batch_size': 256,  # 1024,
               'optimizer': 'adam',
               'adam_lr': 1e-3,
-              'num_users': 18645,
-              'num_items': 9851,
+              'num_users': 10677,
+              'num_items': 2121,
               'latent_dim': 8,
               'num_negative': 4,
               'layers': [16,64,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
               'l2_regularization': 0.0000001,  # MLP model is sensitive to hyper params
               'use_cuda': True,
               'device_id': 0,
+              'device_id_2': 0,
               'pretrain': True,
-              'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4-implict_Epoch49_HR0.4618_NDCG0.6392.model'),
+              'pretrain_mf': 'checkpoints/{}'.format('gmf_netflix_Epoch49_HR0.0645_NDCG0.0929.model'),
               'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
 
 neumf_config = {'alias': 'neumf_netflix',
@@ -223,8 +225,8 @@ neumf_config = {'alias': 'neumf_netflix',
                 'batch_size': 1024,
                 'optimizer': 'adam',
                 'adam_lr': 1e-3,
-                'num_users': 18645,
-                'num_items': 9851,
+                'num_users': 10677,
+                'num_items': 2121,
                 'latent_dim_mf': 8,
                 'latent_dim_mlp': 8,
                 'num_negative': 4,
@@ -232,15 +234,75 @@ neumf_config = {'alias': 'neumf_netflix',
                 'l2_regularization': 0.01,
                 'use_cuda': True,
                 'device_id': 0,
+                'device_id_2': 0,
                 'pretrain': True,
-                'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4-implict_Epoch49_HR0.4618_NDCG0.6392.model'),
+                'pretrain_mf': 'checkpoints/{}'.format('gmf_netflix_Epoch49_HR0.0645_NDCG0.0929.model'),
                 'pretrain_mlp': 'checkpoints/{}'.format('mlp_factor8neg4_bz256_166432168_pretrain_reg_0.0000001_Epoch49_HR0.4790_NDCG0.6568.model'),
                 'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'
                 }
 ##################################################################################################################
 ##################################################################################################################
 # moviesdat config
+gmf_config = {'alias': 'gmf_moviesdat',
+              'num_epoch': 50,
+              'batch_size': 1024,
+              # 'optimizer': 'sgd',
+              # 'sgd_lr': 1e-3,
+              # 'sgd_momentum': 0.9,
+              # 'optimizer': 'rmsprop',
+              # 'rmsprop_lr': 1e-3,
+              # 'rmsprop_alpha': 0.99,
+              # 'rmsprop_momentum': 0,
+              'optimizer': 'adam',
+              'adam_lr': 1e-3,
+              'num_users': 11142,
+              'num_items': 2949,
+              'latent_dim': 8,
+              'num_negative': 4,
+              'l2_regularization': 0, # 0.01
+              'use_cuda': True,
+              'device_id': 3,
+              'device_id_2': 3,
+              'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
 
+mlp_config = {'alias': 'mlp_moviesdat',
+              'num_epoch': 50,
+              'batch_size': 256,  # 1024,
+              'optimizer': 'adam',
+              'adam_lr': 1e-3,
+              'num_users': 11142,
+              'num_items': 2949,
+              'latent_dim': 8,
+              'num_negative': 4,
+              'layers': [16,64,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
+              'l2_regularization': 0.0000001,  # MLP model is sensitive to hyper params
+              'use_cuda': True,
+              'device_id': 3,
+              'device_id_2': 3,
+              'pretrain': True,
+              'pretrain_mf': 'checkpoints/{}'.format('gmf_netflix_Epoch49_HR0.0645_NDCG0.0929.model'),
+              'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
+
+neumf_config = {'alias': 'neumf_moviesdat',
+                'num_epoch': 50,
+                'batch_size': 1024,
+                'optimizer': 'adam',
+                'adam_lr': 1e-3,
+                'num_users': 11142,
+                'num_items': 2949,
+                'latent_dim_mf': 8,
+                'latent_dim_mlp': 8,
+                'num_negative': 4,
+                'layers': [16,64,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
+                'l2_regularization': 0.01,
+                'use_cuda': True,
+                'device_id': 3,
+                'device_id_2': 3,
+                'pretrain': True,
+                'pretrain_mf': 'checkpoints/{}'.format('gmf_netflix_Epoch49_HR0.0645_NDCG0.0929.model'),
+                'pretrain_mlp': 'checkpoints/{}'.format('mlp_factor8neg4_bz256_166432168_pretrain_reg_0.0000001_Epoch49_HR0.4790_NDCG0.6568.model'),
+                'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'
+                }
 ##################################################################################################################
 ##################################################################################################################
 # yahoo_all config
--- src/train.py
+++ src/train.py
@@ -7,7 +7,7 @@ from neumf import NeuMFEngine
 from data import SampleGenerator
 # os.environ['CUDA_VISIBLE_DEVICES'] = '0, 3'
 
-gmf_config = {'alias': 'gmf_netflix',
+gmf_config = {'alias': 'gmf_moviesdat',
               'num_epoch': 50,
               'batch_size': 1024,
               # 'optimizer': 'sgd',
@@ -19,14 +19,14 @@ gmf_config = {'alias': 'gmf_netflix',
               # 'rmsprop_momentum': 0,
               'optimizer': 'adam',
               'adam_lr': 1e-3,
-              'num_users': 10677,
-              'num_items': 2121,
+              'num_users': 11142,
+              'num_items': 2949,
               'latent_dim': 8,
               'num_negative': 4,
               'l2_regularization': 0, # 0.01
               'use_cuda': True,
-              'device_id': 0,
-              'device_id_2': 0,
+              'device_id': 3,
+              'device_id_2': 3,
               'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
 
 mlp_config = {'alias': 'mlp_netflix',
@@ -73,7 +73,8 @@ neumf_config = {'alias': 'neumf_netflix',
 # ml1m_dir = 'data/amazonbeauty_corpus.csv'
 # ml1m_dir = 'data/goodbooks_corpus.csv'
 # ml1m_dir = 'data/yahoo_all_corpus.csv'
-ml1m_dir = 'data/netflix_corpus.csv'
+# ml1m_dir = 'data/netflix_corpus.csv'
+ml1m_dir = 'data/moviesdat_corpus.csv'
 # ml1m_rating = pd.read_csv(ml1m_dir, sep='::', header=None, names=['uid', 'mid', 'rating', 'timestamp'],  engine='python')
 ml1m_rating = pd.read_csv(ml1m_dir, sep=',', header=None, names=['uid', 'mid', 'rating', 'timestamp'],  engine='python')
 # Reindex
@@ -90,10 +91,10 @@ print('Range of itemId is [{}, {}]'.format(ml1m_rating.itemId.min(), ml1m_rating
 sample_generator = SampleGenerator(ratings=ml1m_rating)
 evaluate_data = sample_generator.evaluate_data
 # Specify the exact model
-# config = gmf_config
-# engine = GMFEngine(config)
-config = mlp_config
-engine = MLPEngine(config)
+config = gmf_config
+engine = GMFEngine(config)
+# config = mlp_config
+# engine = MLPEngine(config)
 # config = neumf_config
 # engine = NeuMFEngine(config)
 for epoch in range(config['num_epoch']):
