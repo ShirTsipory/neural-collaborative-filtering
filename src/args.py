fa417440d27c6045dc164ad534e454b4735e1e61
shirtsipory <shirtsipory@gmail.com>
2021-08-16 16:04:53 +0300

    Yahoo 1.

--- src/args.py
+++ src/args.py
@@ -335,7 +335,7 @@ mlp_config = {'alias': 'mlp_yahoo',
               'use_cuda': True,
               'device_id': 0,
               'pretrain': True,
-              'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4-implict_Epoch49_HR0.4618_NDCG0.6392.model'),
+              'pretrain_mf': 'checkpoints/{}'.format(''),
               'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
 
 neumf_config = {'alias': 'neumf_yahoo',
@@ -353,8 +353,8 @@ neumf_config = {'alias': 'neumf_yahoo',
                 'use_cuda': True,
                 'device_id': 0,
                 'pretrain': True,
-                'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4-implict_Epoch49_HR0.4618_NDCG0.6392.model'),
-                'pretrain_mlp': 'checkpoints/{}'.format('mlp_factor8neg4_bz256_166432168_pretrain_reg_0.0000001_Epoch49_HR0.4790_NDCG0.6568.model'),
+                'pretrain_mf': 'checkpoints/{}'.format(''),
+                'pretrain_mlp': 'checkpoints/{}'.format(''),
                 'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'
                 }
 ##################################################################################################################
--- src/train.py
+++ src/train.py
@@ -7,7 +7,7 @@ from neumf import NeuMFEngine
 from data import SampleGenerator
 # os.environ['CUDA_VISIBLE_DEVICES'] = '0, 3'
 
-gmf_config = {'alias': 'gmf_moviesdat',
+gmf_config = {'alias': 'gmf_yahoo',
               'num_epoch': 50,
               'batch_size': 1024,
               # 'optimizer': 'sgd',
@@ -19,59 +19,59 @@ gmf_config = {'alias': 'gmf_moviesdat',
               # 'rmsprop_momentum': 0,
               'optimizer': 'adam',
               'adam_lr': 1e-3,
-              'num_users': 11142,
-              'num_items': 2949,
+              'num_users': 19151,
+              'num_items': 17711,
               'latent_dim': 8,
               'num_negative': 4,
               'l2_regularization': 0, # 0.01
               'use_cuda': True,
-              'device_id': 3,
+              'device_id': 2,
               'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
 
-mlp_config = {'alias': 'mlp_moviesdat',
+mlp_config = {'alias': 'mlp_yahoo',
               'num_epoch': 50,
               'batch_size': 256,  # 1024,
               'optimizer': 'adam',
               'adam_lr': 1e-3,
-              'num_users': 11142,
-              'num_items': 2949,
+              'num_users': 19151,
+              'num_items': 17711,
               'latent_dim': 8,
               'num_negative': 4,
               'layers': [16,64,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
               'l2_regularization': 0.0000001,  # MLP model is sensitive to hyper params
               'use_cuda': True,
-              'device_id': 3,
+              'device_id': 2,
               'pretrain': True,
-              'pretrain_mf': 'checkpoints/{}'.format('gmf_moviesdat_Epoch45_HR0.0066_NDCG0.0125.model'),
+              'pretrain_mf': 'checkpoints/{}'.format(''),
               'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'}
 
-neumf_config = {'alias': 'neumf_moviesdat',
+neumf_config = {'alias': 'neumf_yahoo',
                 'num_epoch': 50,
                 'batch_size': 1024,
                 'optimizer': 'adam',
                 'adam_lr': 1e-3,
-                'num_users': 11142,
-                'num_items': 2949,
+                'num_users': 19151,
+                'num_items': 17711,
                 'latent_dim_mf': 8,
                 'latent_dim_mlp': 8,
                 'num_negative': 4,
                 'layers': [16,64,32,16,8],  # layers[0] is the concat of latent user vector & latent item vector
                 'l2_regularization': 0.01,
                 'use_cuda': True,
-                'device_id': 3,
+                'device_id': 2,
                 'pretrain': True,
-                'pretrain_mf': 'checkpoints/{}'.format('gmf_moviesdat_Epoch45_HR0.0066_NDCG0.0125.model'),
-                'pretrain_mlp': 'checkpoints/{}'.format('mlp_moviesdat_Epoch25_HR0.0066_NDCG0.0122.model'),
+                'pretrain_mf': 'checkpoints/{}'.format(''),
+                'pretrain_mlp': 'checkpoints/{}'.format(''),
                 'model_dir':'checkpoints/{}_Epoch{}_HR{:.4f}_NDCG{:.4f}.model'
                 }
 
 # Load Data
 # ml1m_dir = 'data/movielens_corpus.csv'
+# ml1m_dir = 'data/netflix_corpus.csv'
+# ml1m_dir = 'data/moviesdat_corpus.csv'
+ml1m_dir = 'data/yahoo_all_corpus.csv'
 # ml1m_dir = 'data/amazonbeauty_corpus.csv'
 # ml1m_dir = 'data/goodbooks_corpus.csv'
-# ml1m_dir = 'data/yahoo_all_corpus.csv'
-# ml1m_dir = 'data/netflix_corpus.csv'
-ml1m_dir = 'data/moviesdat_corpus.csv'
 ml1m_rating = pd.read_csv(ml1m_dir, sep=',', header=None, names=['uid', 'mid', 'rating', 'timestamp'],  engine='python')
 # Reindex
 user_id = ml1m_rating[['uid']].drop_duplicates().reindex()
@@ -80,11 +80,11 @@ ml1m_rating = pd.merge(ml1m_rating, user_id, on=['uid'], how='left')
 item_id = ml1m_rating[['mid']].drop_duplicates()
 item_id['itemId'] = np.arange(len(item_id))
 ml1m_rating = pd.merge(ml1m_rating, item_id, on=['mid'], how='left')
-# ml1m_rating.to_csv(r'./csvs/movielens_csv/new_index_movielens.csv', encoding='utf-8', index=False)  #################
-ml1m_rating.to_csv(r'./csvs/moviesdat_csv/new_index_moviesdat.csv', encoding='utf-8', index=False)
+# ml1m_rating.to_csv(r'./csvs/movielens_csv/new_index_movielens.csv', encoding='utf-8', index=False)
 # ml1m_rating.to_csv(r'./csvs/netflix_csv/new_index_netflix.csv', encoding='utf-8', index=False)
-# ml1m_rating.to_csv(r'./csvs/goodbooks_csv/new_index_goodbooks.csv', encoding='utf-8', index=False)
+# ml1m_rating.to_csv(r'./csvs/moviesdat_csv/new_index_moviesdat.csv', encoding='utf-8', index=False)
 # ml1m_rating.to_csv(r'./csvs/amazonbeauty_csv/new_index_amazonbeauty.csv', encoding='utf-8', index=False)
+# ml1m_rating.to_csv(r'./csvs/goodbooks_csv/new_index_goodbooks.csv', encoding='utf-8', index=False)
 ml1m_rating = ml1m_rating[['userId', 'itemId', 'rating', 'timestamp']]
 print('Range of userId is [{}, {}]'.format(ml1m_rating.userId.min(), ml1m_rating.userId.max()))
 print('Range of itemId is [{}, {}]'.format(ml1m_rating.itemId.min(), ml1m_rating.itemId.max()))
@@ -92,12 +92,12 @@ print('Range of itemId is [{}, {}]'.format(ml1m_rating.itemId.min(), ml1m_rating
 sample_generator = SampleGenerator(ratings=ml1m_rating)
 evaluate_data = sample_generator.evaluate_data
 # Specify the exact model
-# config = gmf_config
-# engine = GMFEngine(config)
+config = gmf_config
+engine = GMFEngine(config)
 # config = mlp_config
 # engine = MLPEngine(config)
-config = neumf_config
-engine = NeuMFEngine(config)
+# config = neumf_config
+# engine = NeuMFEngine(config)
 for epoch in range(config['num_epoch']):
     print('Epoch {} starts !'.format(epoch))
     print('-' * 80)
